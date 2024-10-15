621c12c34dad4f7f57e0ea432c9a0cef9d08fd5b
friscasavira <friscasavira724gmail.com>
2024-10-15 15:36:19 +0700

    update fungsi login admin

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
@@ -11,4 +12,21 @@ class AdminLoginController extends Controller
     {
         return view('auth.admin_login');
     }
+
+    public function auth(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (Auth::guard('admin')-> attempt($credentials)) {
+            return redirect()->route('admin.dashboard');
+
+        }
+
+        return back()->withErrors(['login_eror' => 'Username atau Password Salah.'])->onlyInput('username');
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Auth\AdminLoginController;
+use App\Models\Admin\Admin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +10,6 @@ Route::get('/', function () {
 });
 
 route::get('/admin/login', [AdminLoginController::class, 'login'])->name('admin.login');
+route::post('/admin/login', [AdminLoginController::class, 'auth'])->name('admin.auth');
+
+route::get('/admin/dashboard', [AdminController::class,'dashboard'])->name('admin.dashboard');
